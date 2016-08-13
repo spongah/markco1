afc67a9a74d8829a9545a13fcf7c89e33e45762d
spongah <rocksolid801@gmail.com>
2016-08-13 13:37:31 -0600

    update icon and add lat and lng to user table

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
+	before_action :authenticate_user!
   def index
   	gon.active = true;
+  	gon.user_signed_in = user_signed_in?
+
   end
 end
---
+++ db/migrate/20160813185652_add_lat_lng_to_user.rb
@@ -0,0 +1,6 @@
+class AddLatLngToUser < ActiveRecord::Migration
+  def change
+  	add_column :users, :lat, :decimal, {:precision=>10, :scale=>6}
+		add_column :users, :lng, :decimal, {:precision=>10, :scale=>6}
+  end
+end
