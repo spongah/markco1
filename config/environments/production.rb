df618eaf30c80a5f2c4689a2542d8934214278fe
spongah <rocksolid801@gmail.com>
2016-08-30 22:45:24 -0600

    adding confirmable with test mail credentials, hidden env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,25 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_mailer.default_url_options = { :host => 'markco1.herokuapp.com' }
+
+  Rails.application.routes.default_url_options[:host] = 'markco1.herokuapp.com'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
